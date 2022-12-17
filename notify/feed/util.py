3163def760bb2b7966e5a583598c43e2a3fea311
ihfazhillah <mihfazhillah@gmail.com>
2022-12-17 22:05:59 +0700

    add parse method

---
+++ notify/feed/util.py
@@ -0,0 +1,31 @@
+from notify.feed.models import Tag, Item
+import feedparser
+
+
+def parse(tag: Tag):
+    """
+    :param tag: Tag object
+    :return: new entries list. If nothing, just response an empty list
+    """
+
+    resp = feedparser.parse(tag.url)
+    resp.entries.reverse()
+
+    new_entries = []
+
+    for entry in resp.entries:
+        item, created = Item.objects.get_or_create(
+            guid=entry.guid,
+            defaults={
+                "title": entry.title,
+                "content": entry.summary
+            }
+        )
+
+        if not item.tags.filter(pk=tag.id).exists():
+            item.tags.add(tag)
+
+        if created:
+            new_entries.append(item)
+
+    return new_entries
