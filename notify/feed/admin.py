a7eee1089589cfd6097044b4e4ff72da31e6bb1f
ihfazhillah <mihfazhillah@gmail.com>
2022-12-17 22:07:51 +0700

    Add models into admin

--- notify/feed/admin.py
+++ notify/feed/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from notify.feed.models import Tag, Item
 
 # Register your models here.
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["title", "url"]
+
+
+admin.site.register(Tag, TagAdmin)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["tags", "guid", "title", "description"]
+
+
+admin.site.register(Item, ItemAdmin)
