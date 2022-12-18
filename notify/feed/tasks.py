a1e781954438e481ba054e3675bb7e95643969f1
IbnuAmin <mihfazhillah@gmail.com>
2022-12-18 22:27:13 +0700

    Update tasks.py
    Fix malformed topic name

--- notify/feed/tasks.py
+++ notify/feed/tasks.py
@@ -24,7 +24,7 @@ def parse_feeds():
                     "guid": item.guid,
                     "tags": ", ".join([tag.title for tag in item.tags.all()])
                 },
-                topic="job alert",
+                topic="job_alert",
                 notification=Notification(
                     title=item.title, body=f"New job alert: {item.title}"
                 )
