c5bff9ea3209f42d5201abd054eb668460c78b18
ihfazhillah <mihfazhillah@gmail.com>
2022-12-21 23:23:41 +0700

    simplify list response

--- notify/feed/api/serializers.py
+++ notify/feed/api/serializers.py
@@ -12,23 +12,77 @@ class TagSerializer(serializers.ModelSerializer):
 
 class ItemSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
+    budget = serializers.SerializerMethodField()
+    hourly_range = serializers.SerializerMethodField()
+    proposal_example_text = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    country = serializers.SerializerMethodField()
+    skills = serializers.SerializerMethodField()
+
     class Meta:
         model = Item
         fields = [
             "pk",
             "guid",
-            "content",
             "title",
             "published",
             "tags",
-            "accessed",
-            "parsed_item"
+            "description",
+            "budget",
+            "hourly_range",
+            "category",
+            "country",
+            "skills",
+            "proposal_example_text"
         ]
 
+    def get_budget(self, obj):
+        return obj.budget
+
+    def get_hourly_range(self, obj):
+        if not obj.hourly_range:
+            return
+        return [obj.hourly_range.lower, obj.hourly_range.upper]
+
+    def get_description(self, obj):
+        return obj.description
+
+    def get_category(self, obj):
+        return obj.category
+
+    def get_country(self, obj):
+        return obj.country
+
+    def get_skills(self, obj):
+        return obj.skills
+
+    def get_proposal_example_text(self, obj):
+        return obj.proposal_example_text
+
+
+class SimpleItemSerializer(serializers.ModelSerializer):
+    budget = serializers.SerializerMethodField()
+    hourly_range = serializers.SerializerMethodField()
     accessed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Item
+        fields = (
+            "pk",
+            "title",
+            "budget",
+            "hourly_range",
+            "accessed"
+        )
+
+    def get_budget(self, obj):
+        return obj.budget
+
+    def get_hourly_range(self, obj):
+        if not obj.hourly_range:
+            return
+        return [obj.hourly_range.lower, obj.hourly_range.upper]
+
     def get_accessed(self, obj):
         return obj.accessed
-
-    parsed_item = serializers.SerializerMethodField()
-    def get_parsed_item(self, obj):
-        return parse_upwork_feed(obj.content)
--- notify/feed/api/views.py
+++ notify/feed/api/views.py
@@ -1,17 +1,23 @@
 # list feed paginated
 # log access
+from django.contrib.postgres.aggregates import ArrayAgg
 from django.db import models
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from notify.feed.api.serializers import ItemSerializer
+from notify.feed.api.serializers import ItemSerializer, SimpleItemSerializer
 from notify.feed.models import Item, ItemAccess
 
 
 class ItemViewSet(ReadOnlyModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return SimpleItemSerializer
+        return super().get_serializer_class()
 
     def get_queryset(self):
         qs = super().get_queryset().order_by("-created")
@@ -21,8 +27,15 @@ class ItemViewSet(ReadOnlyModelViewSet):
                 models.When(access_count__gt=0, then=models.Value(True)),
                 default=models.Value(False),
                 output_field=models.BooleanField()
-            )
-
+            ),
+            proposal_example_text=models.F("proposal_example__text"),
+            description=models.F("upwork__description"),
+            budget=models.F("upwork__budget"),
+            hourly_range=models.F("upwork__hourly_range"),
+            category=models.F("upwork__category__name"),
+            posted_on=models.F("upwork__posted_on"),
+            country=models.F("upwork__country"),
+            skills=ArrayAgg(models.F("upwork__skills__name"))
         )
         return qs
 
