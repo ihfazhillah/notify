a25a8d69185eaabb48a54b065941dbf8c99f6875
ihfazhillah <mihfazhillah@gmail.com>
2022-12-31 20:57:15 +0700

    Add general prompt to create custom features

--- notify/prompt/admin.py
+++ notify/prompt/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from notify.prompt.models import ProposalPrompt
+from notify.prompt.models import ProposalPrompt, GeneralPrompt
 
 admin.site.register(ProposalPrompt, admin.ModelAdmin)
+admin.site.register(GeneralPrompt, admin.ModelAdmin)
--- notify/prompt/models.py
+++ notify/prompt/models.py
@@ -17,3 +17,14 @@ class ProposalPrompt(TimeStampedModel):
 
     def __str__(self):
         return self.label
+
+
+class GeneralPrompt(TimeStampedModel):
+    """
+    Used for general prompt. the type should unique.
+    """
+    prompt_type = models.CharField(max_length=255, unique=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.prompt_type
